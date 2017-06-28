521ed7b649e768a2cdd7b53a7acfa9b309a6e4c4
huanghai <dusksoft@qq.com>
2017-06-28 22:00:05 +0800

    基础数据类型转换

--- demo-12.cpp
+++ demo-12.cpp
@@ -8,9 +8,9 @@ int main()
 	cout << "char\t\t" << sizeof(char) << "byte\tbit=" << sizeof(char) * 8 << endl;
 	cout << "wchar_t\t\t" <<sizeof(wchar_t) << "byte\tbit" <<  sizeof(wchar_t) * 8 << endl;
 	
-	// Unicode字符集类型，C++11才支持的数据类型,g++编译需要加 -std=c++0x才能编译通过
-	//cout << "wchar_t\t\t" <<sizeof(char16_t) << "byte\tbit" <<  sizeof(char16_t) * 8 << endl;
-	//cout << "wchar_t\t\t" <<sizeof(char32_t) << "byte\tbit" <<  sizeof(char32_t) * 8 << endl;
+	// Unicode字符集类型，C++11才支持的数据类型,部分编译器需要开启-std=c++0x才能编译通过
+	cout << "char16_t\t" <<sizeof(char16_t) << "byte\tbit" <<  sizeof(char16_t) * 8 << endl;
+	cout << "char32_t\t" <<sizeof(char32_t) << "byte\tbit" <<  sizeof(char32_t) * 8 << endl;
 	
 	cout << "short\t\t" <<sizeof(short) << "byte\tbit=" <<  sizeof(short) * 8 << endl;
 	cout << "int\t\t" <<sizeof(int) << "byte\tbit=" <<  sizeof(int) * 8 << endl;
