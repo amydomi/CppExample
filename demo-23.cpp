a7f2e004a14266acbec152d69c6713f407d08080
huanghai <dusksoft@qq.com>
2017-07-24 11:42:49 +0800

    指针数组和数组指针

--- demo-23.cpp
+++ demo-23.cpp
@@ -1,8 +1,22 @@
+/*
+ * 数组
+ * int a[10];		// 存放10个int类型值的数组
+ *
+ * 指针数组 
+ * int *a[10];		// 存放10个int类型指针的数组
+ *
+ * 数组指针
+ * int (*a)[10];	// 指向10个int类型数组的指针
+ *
+ * 数组引用
+ * int (&a)[10];	// 10个int类型数组的引用
+ */
+ 
 #include <iostream>
 #include <iterator>
 
 // 返回指向数组的指针
-int (*getIntArrayPoint())[5]
+int (*getIntArrayPointer())[5]
 {
 	// 因为不能返回局部变量指针，所以声明成static变量
 	static int arr[5] = {1, 2, 3, 4, 5};
@@ -18,8 +32,8 @@ int (&getIntArrayQuote())[5]
 
 int main()
 {
-	// 编历指向数组的指针
-	int (*arr)[5] = getIntArrayPoint();
+	// 遍历指向数组的指针
+	int (*arr)[5] = getIntArrayPointer();
 	for (auto it = std::begin(*arr); it != std::end(*arr); ++it)
 	{
 		std::cout << *it << std::endl;
